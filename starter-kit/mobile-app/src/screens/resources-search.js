cce13dd93dbf607e3d89c49773f0994a9e9153b0
Mayuri Thakur <mayuri.thakur@startv.com>
2020-06-14 18:43:46 +0530

    search. details card

--- starter-kit/mobile-app/src/screens/resources-search.js
+++ starter-kit/mobile-app/src/screens/resources-search.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, FlatList, View, TouchableOpacity, Alert } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  FlatList,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import PickerSelect from 'react-native-picker-select';
 
-import { search } from '../lib/utils';
+import {search} from '../lib/utils';
+import {Card} from 'react-native-shadow-cards';
 
 const styles = StyleSheet.create({
   outerView: {
     backgroundColor: '#FFF',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   inputsView: {
     backgroundColor: '#F1F0EE',
@@ -19,19 +28,19 @@ const styles = StyleSheet.create({
     fontFamily: 'IBMPlexSans-Medium',
     color: '#000',
     fontSize: 14,
-    paddingBottom: 5
+    paddingBottom: 5,
   },
   selector: {
     fontFamily: 'IBMPlexSans-Medium',
     backgroundColor: '#fff',
     padding: 8,
-    marginBottom: 10
+    marginBottom: 10,
   },
   textInput: {
     fontFamily: 'IBMPlexSans-Medium',
     backgroundColor: '#fff',
     padding: 8,
-    marginBottom: 10
+    marginBottom: 10,
   },
   button: {
     backgroundColor: '#ff8c00',
@@ -40,16 +49,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     overflow: 'hidden',
     padding: 12,
-    textAlign:'center',
-    marginTop: 15
+    textAlign: 'center',
+    marginTop: 15,
   },
   searchResultText: {
     fontFamily: 'IBMPlexSans-Bold',
-    padding: 10,
-    color: '#ff7700'
+    padding: 5,
+    color: 'gray',
   },
   flatListView: {
-    backgroundColor: '#FFF'
+    backgroundColor: '#FFF',
   },
   itemTouchable: {
     flexDirection: 'column',
@@ -57,11 +66,11 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'stretch',
     borderBottomColor: '#dddddd',
-    borderBottomWidth: 0.25
+    borderBottomWidth: 0.25,
   },
   itemView: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   itemName: {
     fontSize: 24,
@@ -70,24 +79,27 @@ const styles = StyleSheet.create({
   itemQuantity: {
     fontSize: 14,
     fontFamily: 'IBMPlexSans-Medium',
-    color: 'gray'
+    color: 'gray',
   },
   itemDescription: {
     fontSize: 14,
     fontFamily: 'IBMPlexSans-Medium',
-    color: 'gray'
-  }
+    color: 'gray',
+  },
 });
 
-const SearchResources = function ({ route, navigation }) {
-  const [query, setQuery] = React.useState({ type: 'Food', name: '' });
+const SearchResources = function({route, navigation}) {
+  const [query, setQuery] = React.useState({type: 'Food', name: ''});
   const [items, setItems] = React.useState([]);
   const [info, setInfo] = React.useState('');
 
-  const Item = (props) => {
+  const Item = props => {
     return (
-      <TouchableOpacity style={styles.itemTouchable}
-          onPress={() => { navigation.navigate('Map', { item: props }); }}>
+      <TouchableOpacity
+        style={styles.itemTouchable}
+        onPress={() => {
+          navigation.navigate('Map', {item: props});
+        }}>
         <View style={styles.itemView}>
           <Text style={styles.itemName}>{props.name}</Text>
           <Text style={styles.itemQuantity}> ( {props.quantity} ) </Text>
@@ -99,32 +111,39 @@ const SearchResources = function ({ route, navigation }) {
 
   const searchItem = () => {
     const payload = {
-      ...query
+      ...query,
     };
 
     search(payload)
-      .then((results) => {
-        setInfo(`${results.length} result(s)`)
+      .then(results => {
+        setInfo(`${results.length} result(s)`);
         setItems(results);
       })
       .catch(err => {
         console.log(err);
-        Alert.alert('ERROR', 'Please try again. If the problem persists contact an administrator.', [{text: 'OK'}]);
+        Alert.alert(
+          'ERROR',
+          'Please try again. If the problem persists contact an administrator.',
+          [{text: 'OK'}],
+        );
       });
   };
-
+  const users = [
+    {Name: 'Mayuri', Address: 'Mumbai', Contact: '12349585849'},
+    {Name: 'Ankita', Address: 'Mumbai', Contact: '12349585849'},
+  ];
   return (
     <View style={styles.outerView}>
       <View style={styles.inputsView}>
         <Text style={styles.label}>Type</Text>
         <PickerSelect
-          style={{ inputIOS: styles.selector }}
+          style={{inputIOS: styles.selector}}
           value={query.type}
-          onValueChange={(t) => setQuery({ ...query, type: t })}
+          onValueChange={t => setQuery({...query, type: t})}
           items={[
-              { label: 'Beneficiary', value: 'beneficiary' },
-              { label: 'Volunteers', value: 'volunteers' },
-              { label: 'Events', value: 'events' }
+            {label: 'Beneficiary', value: 'beneficiary'},
+            {label: 'Volunteers', value: 'volunteers'},
+            {label: 'Events', value: 'events'},
           ]}
         />
 
@@ -133,11 +152,26 @@ const SearchResources = function ({ route, navigation }) {
         </TouchableOpacity>
       </View>
 
-      <Text style={styles.searchResultText}>{info}</Text>
+      <View style={styles.container}>
+        {Object.keys(users).map(key => {
+          return (
+            <Card style={{padding: 10, margin: 10}}>
+              {Object.keys(users[key]).map((item, index) => {
+                return (
+                  <Text key={index} style={styles.searchResultText}>
+                    {item} : {users[key][item]}
+                  </Text>
+                );
+              })}
+            </Card>
+          );
+        })}
+      </View>
 
-      <FlatList style={styles.flatListView}
+      <FlatList
+        style={styles.flatListView}
         data={items}
-        renderItem={({ item }) => <Item {...item} />}
+        renderItem={({item}) => <Item {...item} />}
         keyExtractor={item => item.id || item['_id']}
       />
     </View>
